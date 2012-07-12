d30b04d974cbe175295182ef6db81b1a47c522b2
smiron86@gmail.com <smiron86@gmail.com@96f20ce0-155b-0d30-60b7-226f45b99559>
2012-07-12 20:32:03 +0000

    Tests 2

---
+++ trunk/Test2/train.m
@@ -0,0 +1,60 @@
+function theta = train(patchsize, hiddenSize, numpatches, sparsityParam, lambda , beta)
+
+
+%% main application
+fprintf('\n\n\n------------------------------------------------\n')
+% load images from disk
+load IMAGES;    
+
+visibleSize = patchsize * patchsize;
+
+sparsityParam = 0.01;   % desired average activation of the hidden units.
+                     % (This was denoted by the Greek alphabet rho, which looks like a lower-case "p",
+		     %  in the lecture notes). 
+lambda = 0.0001;     % weight decay parameter       
+beta = 3;            % weight of sparsity penalty term      
+
+
+%startTime = cputime;
+% DO WORK
+%fprintf('sampleIMAGES TIME: %g\n',cputime-startTime);
+
+startTime = cputime;
+
+patches = sampleImages(IMAGES, patchsize, numpatches);
+
+gpatches = gpuArray(patches);
+
+theta = initializeParameters(hiddenSize, visibleSize);
+
+[opttheta, cost] = ...
+    trainCase(theta, gpatches, visibleSize, hiddenSize, ...
+        lambda, sparsityParam, beta);
+
+                          
+%%======================================================================
+%% STEP 5: Visualization 
+W1 = reshape(opttheta(1:hiddenSize*visibleSize), hiddenSize, visibleSize);
+display_network(W1', 12); 
+
+
+
+name = strcat(   int2str(visibleSize) ,'-' , int2str(hiddenSize) , '-' , int2str(visibleSize) ,  ...
+        '-N' , int2str(numpatches) , '-S' , sprintf('%f', sparsityParam) , '-L' , sprintf('%f', lambda) , '-B' , sprintf('%f',beta));
+
+mkdir(  strcat('Results\',name));
+    
+path =  strcat('Results\',name,'\weights.jpg');
+
+print('-djpeg',path);   % save the visualization to a file 
+
+thetaPath =  strcat('Results\',name,'\theta.txt');
+dataSizePath =  strcat('Results\',name,'\dataSize.txt');
+
+totalTime = cputime-startTime;
+
+dlmwrite(thetaPath,opttheta);
+dlmwrite(dataSizePath,[visibleSize,hiddenSize,cost, totalTime]);
+
+fprintf('sampleIMAGES TIME: %g\n',totalTime);
+
---
+++ trunk/Test2/trainCase.m
@@ -0,0 +1,29 @@
+function [opttheta, cost] = ...
+    trainCase(theta, patches, visibleSize, hiddenSize, ...
+        lambda, sparsityParam, beta)
+%% prepare execution
+import parallel.gpu.GPUArray
+
+%  Use minFunc to minimize the function
+addpath minFunc/
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;    % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
+
+%%======================================================================
+%% minimize
+try
+    [opttheta, cost] = minFunc( @(p) sparseAutoencoderCostGpu(p, ...
+                                       visibleSize, hiddenSize, ...
+                                       lambda, sparsityParam, ...
+                                       beta, patches), ...
+                                  theta, options);
+                              
+catch exception
+    disp(exception)
+end
+%%======================================================================
